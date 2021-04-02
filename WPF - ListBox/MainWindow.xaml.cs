f29004ade63447dd0b35f6930fb72a4e3420028b
Drew <dreww@mortgage365.com>
2021-04-02 11:51:53 -0600

    150-155: Did not code along, just watched - not really interested in FE styling

--- WPF - ListBox/MainWindow.xaml.cs
+++ WPF - ListBox/MainWindow.xaml.cs
@@ -26,13 +26,4 @@ namespace WPF___ListBox
             comboBoxColors.ItemsSource = typeof(Colors).GetProperties();
         }
     }
-
-    public class Match
-    {
-        public int Score1 { get; set; }
-        public int Score2 { get; set; }
-        public string Team1 { get; set; }
-        public string Team2 { get; set; }
-        public int Completion { get; set; }
-    }
 }
